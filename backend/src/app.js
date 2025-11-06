1d8e17c2f388011d106f0b4caf38bb1163c28c65
Hassan-Adelani-Luqman <h.luqman@alustudent.com>
2025-11-06 13:35:58 +0200

    feat: prepare for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes/notesRoutes.js";
+import path from "node:path"
 const app = express();
 import { connectDB } from "./config/db.js";
 import { connect } from "mongoose";
@@ -10,9 +11,25 @@ import cors from "cors"
 dotenv.config();
 const PORT = 5000;
 
-app.use(cors());
+//to get the root directory
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors(
+        {
+            origin: "http://localhost:5173"
+        }
+    ));
+}
 app.use(express.json())
 app.use(ratelimiter);
-console.log(process.env.MONGO_URI);
 app.use("/api/notes", router);
+
+if(process.env.NODE_ENV  === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 connectDB().then(()=> app.listen(PORT, () => console.log("listening for requests")));
